84e3943013c2cb4c34e4c9dec91f3db915f72492
MultiHost2020 <69394394+MultiHost2020@users.noreply.github.com>
2020-09-01 19:56:42 +0200

    init

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on("ready", async () => {
 
   console.log(`${client.user.username} is online.`);
 
-  client.user.setActivity("Do not use!", { type: "PLAYING" });
+  client.user.setActivity("!help", { type: "PLAYING" });
 
 });
 
